3f5e83acdfe91eac10ef3a8a62f3cd3d8ede4fd7
Philip-Dryden <dryden.github@posteo.de>
2024-12-25 22:46:26 +0100

    Create Weapon Class

---
+++ RPG/src/game/Weapon.java
@@ -0,0 +1,30 @@
+package game;
+
+public class Weapon {
+
+	private int id;
+	private String name;
+	private int damage;
+	private int requiredStrength;
+	private int requiredIntelligence;
+	private int requiredLevel;
+	
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getDamage() {
+		return damage;
+	}
+	public int getRequiredStrength() {
+		return requiredStrength;
+	}
+	public int getRequiredIntelligence() {
+		return requiredIntelligence;
+	}
+	public int getRequiredLevel() {
+		return requiredLevel;
+	}
+}
