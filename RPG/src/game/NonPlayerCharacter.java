8398d945fbcbe8cd318ce7e6754265d5f1228863
Philip-Dryden <dryden.github@posteo.de>
2024-12-26 01:26:00 +0100

    Create NonPlayerCharacter

---
+++ RPG/src/game/NonPlayerCharacter.java
@@ -0,0 +1,12 @@
+package game;
+
+public class NonPlayerCharacter extends Character {
+
+	public NonPlayerCharacter(String name, int level, int maxHealth, int maxMana, int strength, int intelligence) {
+		super(name, level, maxHealth, maxMana, strength, intelligence);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+}
