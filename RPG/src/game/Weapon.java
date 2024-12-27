5b951be6a0b6327314d94b90aa09f1de13f2fc5c
Philip-Dryden <dryden.github@posteo.de>
2024-12-27 18:13:53 +0100

    Refactor: Remove redundant attributes and methods and change Constructor

--- RPG/src/game/Weapon.java
+++ RPG/src/game/Weapon.java
@@ -1,36 +1,16 @@
 package game;
 
-public class Weapon {
+public class Weapon extends Equipment {
 
-	private String name;
 	private int damage;
-	private int requiredStrength;
-	private int requiredIntelligence;
-	private int requiredLevel;
-	
-	public String getName() {
-		return name;
-	}
+
 	public int getDamage() {
 		return damage;
 	}
-	public int getRequiredStrength() {
-		return requiredStrength;
-	}
-	public int getRequiredIntelligence() {
-		return requiredIntelligence;
-	}
-	public int getRequiredLevel() {
-		return requiredLevel;
-	}
-	
+
 	public Weapon(String name, int damage, int requiredStrength, int requiredIntelligence, int requiredLevel) {
-		super();
-		this.name = name;
+		super(name, requiredStrength, requiredIntelligence, requiredLevel);
 		this.damage = damage;
-		this.requiredStrength = requiredStrength;
-		this.requiredIntelligence = requiredIntelligence;
-		this.requiredLevel = requiredLevel;
 	}
 	
 
