4e6dddac5ea0213a56da547edd15f23a8dd50d81
Philip-Dryden <dryden.github@posteo.de>
2024-12-27 18:08:46 +0100

    Create Item class as base class for items

---
+++ RPG/src/game/Item.java
@@ -0,0 +1,23 @@
+package game;
+
+public abstract class Item {
+	
+	protected String name;
+//	protected int value;
+//	protected int stackSize;
+	
+	
+	public String getName() {
+		return name;
+	}
+/*	public int getValue() {
+		return value;
+	}
+	public int getStackSize() {
+		return stackSize;
+	}
+*/	
+	public Item (String name) {
+		this.name	=	name;
+	}
+}
