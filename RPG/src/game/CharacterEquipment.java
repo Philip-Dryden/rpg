afac50f9e9ab2fb3f17570f7317a6aaecb63df27
Philip-Dryden <dryden.github@posteo.de>
2024-12-27 22:50:35 +0100

    Create CharacterEquipment

---
+++ RPG/src/game/CharacterEquipment.java
@@ -0,0 +1,110 @@
+package game;
+
+public class CharacterEquipment {
+	
+	private Armour head;
+	private Armour shoulders;
+	private Armour chest;
+	private Armour wrist;
+	private Armour hands;
+	private Armour waist;
+	private Armour legs;
+	private Armour feet;
+	
+	private Equipment neck;
+	private Equipment back;
+	private Equipment finger;
+	private Equipment trinket;
+	
+	private Weapon mainhand;
+	private Weapon offhand;
+	
+	
+	public Armour getHead() {
+		return head;
+	}
+	public void setHead(Armour head) {
+		this.head = head;
+	}
+	public Armour getShoulders() {
+		return shoulders;
+	}
+	public void setShoulders(Armour shoulders) {
+		this.shoulders = shoulders;
+	}
+	public Armour getChest() {
+		return chest;
+	}
+	public void setChest(Armour chest) {
+		this.chest = chest;
+	}
+	public Armour getWrist() {
+		return wrist;
+	}
+	public void setWrist(Armour wrist) {
+		this.wrist = wrist;
+	}
+	public Armour getHands() {
+		return hands;
+	}
+	public void setHands(Armour hands) {
+		this.hands = hands;
+	}
+	public Armour getWaist() {
+		return waist;
+	}
+	public void setWaist(Armour waist) {
+		this.waist = waist;
+	}
+	public Armour getLegs() {
+		return legs;
+	}
+	public void setLegs(Armour legs) {
+		this.legs = legs;
+	}
+	public Armour getFeet() {
+		return feet;
+	}
+	public void setFeet(Armour feet) {
+		this.feet = feet;
+	}
+	public Equipment getNeck() {
+		return neck;
+	}
+	public void setNeck(Equipment neck) {
+		this.neck = neck;
+	}
+	public Equipment getBack() {
+		return back;
+	}
+	public void setBack(Equipment back) {
+		this.back = back;
+	}
+	public Equipment getFinger() {
+		return finger;
+	}
+	public void setFinger(Equipment finger) {
+		this.finger = finger;
+	}
+	public Equipment getTrinket() {
+		return trinket;
+	}
+	public void setTrinket(Equipment trinket) {
+		this.trinket = trinket;
+	}
+	public Weapon getMainhand() {
+		return mainhand;
+	}
+	public void setMainhand(Weapon mainhand) {
+		this.mainhand = mainhand;
+	}
+	public Weapon getOffhand() {
+		return offhand;
+	}
+	public void setOffhand(Weapon offhand) {
+		this.offhand = offhand;
+	}
+	
+	
+
+}
