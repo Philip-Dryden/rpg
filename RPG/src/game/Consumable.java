4a8fa724b3da884bc3614c2113f725ff2e7a7035
Philip-Dryden <dryden.github@posteo.de>
2024-12-27 18:29:14 +0100

    Create Consumable Class

---
+++ RPG/src/game/Consumable.java
@@ -0,0 +1,15 @@
+package game;
+
+public class Consumable extends Item{
+	
+	private int restoredHealth;
+	private int restoredMana;
+	
+	public Consumable (String name, int restoredHealth, int restoredMana) {
+		super (name);
+		this.restoredHealth	= restoredHealth;
+		this.restoredMana	= restoredMana;
+		
+	}
+
+}
